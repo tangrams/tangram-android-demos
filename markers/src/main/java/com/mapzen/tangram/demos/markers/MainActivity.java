5b9e7cd91b977ab98923877f77f31bf79101bca6
Matt Blair <blair1618@gmail.com>
2016-04-23 16:31:49 -0400

    Add a sample application that edits map markers

---
+++ markers/src/main/java/com/mapzen/tangram/demos/markers/MainActivity.java
@@ -0,0 +1,136 @@
+package com.mapzen.tangram.demos.markers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+
+import com.mapzen.tangram.LngLat;
+import com.mapzen.tangram.MapController;
+import com.mapzen.tangram.MapData;
+import com.mapzen.tangram.MapView;
+import com.mapzen.tangram.TouchInput;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements MapView.OnMapReadyCallback {
+
+    MapController map;
+    MapView view;
+
+    MapData points;
+    MapData lines;
+    MapData polygons;
+
+    MapData current;
+
+    Button clearButton;
+
+    ArrayList<LngLat> taps = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        view = (MapView)findViewById(R.id.map);
+        view.onCreate(savedInstanceState);
+        view.getMapAsync(this, "bubble-wrap.yaml");
+
+        clearButton = (Button)findViewById(R.id.clear_button);
+    }
+
+    @Override
+    public void onMapReady(MapController mapController) {
+        map = mapController;
+
+        points = map.addDataLayer("mz_default_point");
+        lines = map.addDataLayer("mz_default_line");
+        polygons = map.addDataLayer("mz_default_polygon");
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (current != null) {
+                    current.clear();
+                }
+            }
+        });
+
+        map.setTapResponder(new TouchInput.TapResponder() {
+            @Override
+            public boolean onSingleTapUp(float x, float y) {
+                LngLat tap = map.coordinatesAtScreenPosition(x, y);
+                taps.add(tap);
+                if (current == points) {
+                    points.addPoint(tap, null);
+                    taps.clear();
+                } else if (current == lines && taps.size() >= 2) {
+                    lines.addPolyline(taps, null);
+                    taps.remove(0);
+                } else if (current == polygons && taps.size() >= 3) {
+                    ArrayList<List<LngLat>> polygon = new ArrayList<>();
+                    polygon.add(taps);
+                    polygons.addPolygon(polygon, null);
+                    taps.remove(0);
+                }
+                map.requestRender();
+                return false;
+            }
+
+            @Override
+            public boolean onSingleTapConfirmed(float x, float y) {
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        view.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        view.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        view.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        view.onLowMemory();
+    }
+
+    public void onRadioButtonClicked(View view) {
+
+        boolean checked = ((RadioButton) view).isChecked();
+
+        if (!checked) {
+            return;
+        }
+
+        taps.clear();
+
+        switch(view.getId()) {
+            case R.id.radio_points:
+                current = points;
+                break;
+            case R.id.radio_lines:
+                current = lines;
+                break;
+            case R.id.radio_polygons:
+                current = polygons;
+                break;
+        }
+    }
+}
